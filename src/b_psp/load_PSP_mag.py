5540dfdb266ac5ce8271f9e555d4f4ffb82a0075
srijaniiserprinceton <sbdas@princeton.edu>
2024-07-17 11:02:02 -0400

    Added autodoc comments and modified .gitignore to ignore /docs and html files

--- src/b_psp/load_PSP_mag.py
+++ src/b_psp/load_PSP_mag.py
@@ -3,6 +3,23 @@ import matplotlib.pyplot as plt
 import pytplot, pyspedas
 
 def load_B(tstart, tend):
+    '''
+    Function to magnetic fields meaured by PSP for a desired time interval.
+
+    Parameters:
+    -----------
+    tstart: string 'yyyy-mm-dd'
+            Start time in the above format.
+    tend: string 'yyyy-mm-dd'
+            End time in the above format.
+
+    Returns:
+    --------
+    time_fld: array_like of np.datetime64
+                Array containing time epochs.
+    BRTN: array_like of shape (3, Ntimes)
+            Array containing the BR, BT, BN time-series.
+    '''
     # downloading PSP data from FIELDS
     fields_vars = pyspedas.psp.fields(trange=[tstart, tend], varnames=['psp_fld_l2_mag_RTN_4_Sa_per_Cyc'],
                                     datatype='mag_RTN_4_Sa_per_Cyc', level='l2', time_clip=True)
--- src/b_psp/misc_functions.py
+++ src/b_psp/misc_functions.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 def calc_Bmag(BRTN):
+  '''
+  Calculates the strength of magnetic field vector.
+
+  Parameters:
+  -----------
+  BRTN: array_like of floats. Shape (3, Ntimes)
+        Array containing the BR, BT, BN time-series.
+
+  Returns:
+  --------
+  Bmag: array_like of floats. Shape (Ntimes,)
+        Array of magnetic field strength.
+  '''
   BR, BT, BN = BRTN
   Bmag = np.sqrt(BR**2 +  BN**2 + BT**2) 
   return Bmag
