dd797db0675a161ae3fd081ce2aeacc61d978ed8
srijaniiserprinceton <sbdas@princeton.edu>
2024-07-17 10:13:52 -0400

    Loading PSP magnetic field using pyspedas works.

--- load_PSP_mag.py
+++ load_PSP_mag.py
@@ -1,3 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pytplot
+import pytplot, pyspedas
+
+def load_B(tstart, tend):
+    # downloading PSP data from FIELDS
+    fields_vars = pyspedas.psp.fields(trange=[tstart, tend], varnames=['psp_fld_l2_mag_RTN_4_Sa_per_Cyc'],
+                                    datatype='mag_RTN_4_Sa_per_Cyc', level='l2', time_clip=True)
+    # extracting time and BR, BT, BN            
+    time_fld = pytplot.data_quants['psp_fld_l2_mag_RTN_4_Sa_per_Cyc'].time.data
+    B_RTN = pytplot.data_quants['psp_fld_l2_mag_RTN_4_Sa_per_Cyc'].data.T
+
+    return time_fld, B_RTN
+
+if __name__=='__main__':
+    # defining a time window
+    start_time, end_time = '2018-11-5', '2018-11-6'
+    time, BRTN = load_B(start_time, end_time)
